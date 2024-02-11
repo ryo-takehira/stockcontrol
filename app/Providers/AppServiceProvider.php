0986bb618099a011013494ce76adbe880df4c013
ryo-takehira <145681998+ryo-takehira@users.noreply.github.com>
2024-02-11 17:49:28 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (\App::environment(['production'])){
+            \URL::forceScheme('https');
+        }
     }
 }
