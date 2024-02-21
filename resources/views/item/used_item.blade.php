016e79bc1d3e9623cc5d9db879eee5599672db69
ryo-takehira <145681998+ryo-takehira@users.noreply.github.com>
2024-02-22 00:27:34 +0900

    在庫数通知アラート

--- resources/views/item/used_item.blade.php
+++ resources/views/item/used_item.blade.php
@@ -13,105 +13,114 @@
         {{ session('success') }}
     </div>
     @endif
-
 </div>
-@stop
-@section('content')
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <!-- 検索窓 -->
-                <div class="mb-2">
-                    <div class="d-flex">
-                        <form class="d-flex text-align-center" action="{{ url('/items/used_itemsearch') }}" method="get">
-                            <input class="search-window" type="text" name="used_search" placeholder="検索キーワード" value=''>
-                            <button type="submit" class="search-button ms-4">検索</button>
-                        </form>
+
+<!-- 在庫チェック -->
+@foreach ($items as $item)
+@if($item->stock < $item->minimum_stock)
+    <div>
+        <p style="color:red; font-weight:bold;">{{ $item->type}}の【 {{ $item->name}} 】の在庫が最低在庫数に達しています</p><br>
+    </div>
+    @endif
+    @endforeach
+    @stop
+
+    @section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <!-- 検索窓 -->
+                    <div class="mb-2">
+                        <div class="d-flex">
+                            <form class="d-flex text-align-center" action="{{ url('/items/used_itemsearch') }}" method="get">
+                                <input class="search-window" type="text" name="used_search" placeholder="検索キーワード" value=''>
+                                <button type="submit" class="search-button ms-4">検索</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="table-responsive p-0">
+                        <table class="table text-center text-nowrap">
+                            <thead>
+                                <tr>
+                                    <th class="fixed02">ID</th>
+                                    <th class="fixed01">備品名</th>
+                                    <th class="fixed02">部署</th>
+                                    <th class="fixed02">画像</th>
+                                    <th class="fixed02">型番</th>
+                                    <th class="fixed02">在庫単位</th>
+                                    <th class="fixed02">在庫数</th>
+                                    <th class="fixed02">最低在庫数</th>
+                                    <th class="fixed02">発注数</th>
+                                    <th class="fixed02">操作</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($items as $item)
+                                <tr>
+                                    <td class="align-middle">{{ $item->id }}</td>
+                                    <td class="align-middle fixed02">{{ $item->name }}</td>
+                                    <td class="align-middle">{{ $item->type }}</td>
+                                    <td class="align-middle">
+                                        <div style="width:100px;"><img src="{!! $item->image_name !!}" alt="IMAGE" style="width:100%; height:50%;"></div>
+                                    </td>
+                                    <td class="align-middle">{{ $item->model_no }}</td>
+                                    <td class="align-middle">{{ $item->stock_unit}}</td>
+                                    <td class="align-middle">{{ $item->stock}}</td>
+                                    <td class="align-middle">{{ $item->minimum_stock}}</td>
+                                    <td class="align-middle">{{ $item->order_quantity}}</td>
+                                    <td class="align-middle">
+                                        <!-- 持出ボタン -->
+                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#itemModalLabel{{ $item->id }}">
+                                            持出
+                                        </button>
+                                        <!-- 備品持出入力モーダル -->
+                                        @include('modals.item_modal', ['item' => $item])
+                                    </td>
+                    </div>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                    </table>
+                </div>
+
+                <div class="page-link">
+                    <div class="row">
+                        <div class="col-6">
+                            {{ $items->appends(request()->query())->links('pagination::bootstrap-5')}}
+                        </div>
                     </div>
                 </div>
-                <div class="table-responsive p-0">
-                    <table class="table text-center text-nowrap">
-                        <thead>
-                            <tr>
-                                <th class="fixed02">ID</th>
-                                <th class="fixed01">備品名</th>
-                                <th class="fixed02">部署</th>
-                                <th class="fixed02">画像</th>
-                                <th class="fixed02">型番</th>
-                                <th class="fixed02">在庫単位</th>
-                                <th class="fixed02">在庫数</th>
-                                <th class="fixed02">最低在庫数</th>
-                                <th class="fixed02">発注数</th>
-                                <th class="fixed02">操作</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($items as $item)
-                            <tr>
-                                <td class="align-middle">{{ $item->id }}</td>
-                                <td class="align-middle fixed02">{{ $item->name }}</td>
-                                <td class="align-middle">{{ $item->type }}</td>
-                                <td class="align-middle">
-                                    <div style="width:100px;"><img src="{!! $item->image_name !!}" alt="IMAGE" style="width:100%; height:50%;"></div>
-                                </td>
-                                <td class="align-middle">{{ $item->model_no }}</td>
-                                <td class="align-middle">{{ $item->stock_unit}}</td>
-                                <td class="align-middle">{{ $item->stock}}</td>
-                                <td class="align-middle">{{ $item->minimum_stock}}</td>
-                                <td class="align-middle">{{ $item->order_quantity}}</td>
-                                <td class="align-middle">
-                                    <!-- 持出ボタン -->
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#itemModalLabel{{ $item->id }}">
-                                        持出
-                                    </button>
-                                    <!-- 備品持出入力モーダル -->
-                                    @include('modals.item_modal', ['item' => $item])
-                                </td>
-                </div>
-                </tr>
-                @endforeach
-                </tbody>
-                </table>
-            </div>
 
-            <div class="page-link">
-                <div class="row">
-                    <div class="col-6">
-                        {{ $items->appends(request()->query())->links('pagination::bootstrap-5')}}
-                    </div>
-                </div>
             </div>
-
         </div>
     </div>
-</div>
 
 
-<!-- jquery読込 -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function() {
-        // ページロード後に実行されるコード
+    <!-- jquery読込 -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            // ページロード後に実行されるコード
 
-        // メッセージが存在する場合
-        if ($('#successMessage').length) {
-            // メッセージを3秒後にフェードアウト
-            setTimeout(function() {
-                $('#successMessage').fadeOut('slow');
-            }, 3000);
-        }
-    });
-</script>
-@stop
+            // メッセージが存在する場合
+            if ($('#successMessage').length) {
+                // メッセージを3秒後にフェードアウト
+                setTimeout(function() {
+                    $('#successMessage').fadeOut('slow');
+                }, 3000);
+            }
+        });
+    </script>
+    @stop
 
-@section('css')
-<!-- オリジナルstylecssファイル -->
-<link href="{{ asset('/css/item.css') }}" rel="stylesheet">
-@stop
+    @section('css')
+    <!-- オリジナルstylecssファイル -->
+    <link href="{{ asset('/css/item.css') }}" rel="stylesheet">
+    @stop
 
-@section('js')
-<!-- Bootstrap JavaScriptのリンク -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    @section('js')
+    <!-- Bootstrap JavaScriptのリンク -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-@stop
+    @stop
