6d4298904d3027a0ceef6b9300dc748e24af1376
ryo-takehira <145681998+ryo-takehira@users.noreply.github.com>
2024-02-23 17:32:37 +0900

    migrationファイルメモを追加

--- database/migrations/2024_02_23_165043_create_items_tabke.php
+++ database/migrations/2024_02_23_165043_create_items_tabke.php
@@ -12,6 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('items', function (Blueprint $table) {
+            // カラムを追加する場合
+            // $table->string('order_person')->nullable(); 
+            // $table->string('order_phone')->nullable(); 
+
+            // カラム値のnullの値を許容する変更でデータベース更新
             $table->string('order_person')->nullable()->change(); 
             $table->string('order_phone')->nullable()->change(); 
         });
@@ -23,8 +28,13 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('items', function (Blueprint $table) {
+            // データベースにカラム追加の場合
+            // $table->dropColumn('order_person');
+            // $table->dropColumn('order_phone'); 
+
+            // カラム値の値を変更でデータベース更新場合
             $table->string('order_person')->nullable()->change(); 
-            $table->string('order_phone')->nullable()->change();
+            $table->string('order_phone')->nullable()->change(); 
         });
     }
 };
