af14559ef43cdbbdbaf61868493a95f64b9a559a
ryo-takehira <145681998+ryo-takehira@users.noreply.github.com>
2024-03-08 12:52:00 +0900

    edit.blade.php の更新 修正

--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -22,10 +22,13 @@
         <div class="card card-primary">
             <form method="POST" action="{{ url('/items/' . $item->id . '/edit') }}" enctype="multipart/form-data">
                 @csrf
+
+                <input type="hidden" name="item_id" id="item_id" value="{!! $item->id !!}">
+
                 <div class="card-body">
                     <div class="form-group">　　
 
-                    <input type="hidden" name="item_id" id="item_id" value="{!! $item->id !!}">
+                    
 
                         <label for="name">備品名</label>
                         <input type="text" class="form-control" id="name" name="name" placeholder="備品名：{{ $item->name }}" value="{{ $item->name }}">
