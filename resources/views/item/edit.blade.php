a08af506b53e9629fb80f26f45f1dba3ccb3a546
ryo-takehira <145681998+ryo-takehira@users.noreply.github.com>
2024-03-08 17:41:44 +0900

    edit.blade.php の更新

--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -27,7 +27,7 @@
                     <div class="form-group">　　
                         <label for="name">備品名</label>
                      
-                        <div class="form-group">
+                    
                         <input type="hidden" name="item_id" id="item_id" value="{!! $item->id !!}"> 
                         <input type="text" class="form-control" id="name" name="name" placeholder="備品名：{{ $item->name }}" value="{{ $item->name }}">
                     </div>
