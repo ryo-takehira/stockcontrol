00118f28fa2e1326e6b9229b3db202b39d942061
ryo-takehira <145681998+ryo-takehira@users.noreply.github.com>
2024-03-08 12:29:37 +0900

    edit.blade.php の更新 登録ボタンレイアウト検証

--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -23,10 +23,10 @@
             <form method="POST" action="{{ url('/items/' . $item->id . '/edit') }}" enctype="multipart/form-data">
                 @csrf
                 <div class="card-body">
+                    <div class="form-group">　　
 
                     <input type="hidden" name="item_id" id="item_id" value="{!! $item->id !!}">
 
-                    <div class="form-group">
                         <label for="name">備品名</label>
                         <input type="text" class="form-control" id="name" name="name" placeholder="備品名：{{ $item->name }}" value="{{ $item->name }}">
                     </div>
